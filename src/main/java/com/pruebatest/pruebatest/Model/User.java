b7fdcd61acef08d063bdbdf41151cf8bea957595
Braian Rojas <braianrojas95@gmail.com>
2025-05-13 21:46:15 -0300

    fix: renombrada tabla de User para evitar conflicto con palabra reservada en H2

--- src/main/java/com/pruebatest/pruebatest/Model/User.java
+++ src/main/java/com/pruebatest/pruebatest/Model/User.java
@@ -3,9 +3,11 @@ package com.pruebatest.pruebatest.Model;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Table(name = "users")
 public class User {
 
     @Id
